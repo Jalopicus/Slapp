cd64ea196ec88d5921fd2031384b3c68dd48e6fc
Jalopicus <bryan@moodistcolony.com>
2016-04-11 09:07:44 -0400

    Explodey bones.

--- Slashboard/Datality/AshleyGraham.cs
+++ Slashboard/Datality/AshleyGraham.cs
@@ -7,6 +7,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Datality {
+    using ComplexTypes;
     public sealed class AshleyGraham : DbContext {
         // Your context has been configured to use a 'AshleyGraham' connection string from your application's 
         // configuration file (App.config or Web.config). By default, this connection string targets the 
@@ -22,16 +23,9 @@ namespace Datality {
             Toxicities = Set<Toxicity>();
             SpecSets = Set<SpecSet>();
             Safeties = Set<Safety>();
-            //    var dbug = this.Configuration;
-                Database.SetInitializer(new DropCreateDatabaseIfModelChanges<AshleyGraham>());
+            //Wipes database if model changes, suppresses errors nobody asks for
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<AshleyGraham>());
         }
-      //  protected override void OnModelCreating(DbModelBuilder modelBuilder) {
-//            base.OnModelCreating(modelBuilder);
-//            modelBuilder.Entity<Blend>().Map(m => {
- //               m.MapInheritedProperties();
-  //              m.ToTable("Blends");
-   //         });
-     //   }
         // Add a DbSet for each entity type that you want to include in your model. For more information 
         // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
         // public virtual DbSet<MyEntity> MyEntities { get; set; }
@@ -43,37 +37,30 @@ namespace Datality {
         internal DbSet<SpecSet> SpecSets { get; set; }
         internal DbSet<Safety> Safeties { get; set; }
     }
-    //public class MyEntity
-    //{
-    //    public int Id { get; set; }
-    //    public string Name { get; set; }
-    //}
     /// <summary>
     /// Tables/sets will inherit this base class
+    /// 
     /// </summary>
-  
-    public abstract class Bass : INotifyPropertyChanged {
+     public abstract class Bass {//: INotifyPropertyChanged {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         protected string Note { get; set; }
+        /// <summary>
+        /// The entity bass class implementation of INotifyPropertyChanged interface
+        /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
         protected void OnPc(string propertyName) {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
-    #region Chemical Entities
-    /// <summary>
-    /// Chemical base
-    /// </summary>
-  
     public abstract class Chemical : Bass {
    //     protected Chemical() {
    //         this.Toxicities = new HashSet<Toxicity>();
    //     }
        // public string Name { get { return _name; } set { _name = value; OnPc("Name"); } }
      //   private string _name;
-     public string Name { get; set; }
+        public string Name { get; set; }
         public virtual Reggo Reg { get; set; }
         public virtual ICollection<Toxicity> Toxicities { get; set; }
     }
@@ -81,7 +68,6 @@ namespace Datality {
     /// Raw material
     /// </summary>
     public class Raw : Bass {
-
         // public string StockConcentration { get { return _stockConcentration; } set { _stockConcentration = value; OnPc("StockConcentration"); } }
         // private string _stockConcentration = null;
         // public string Cas { get { return _cas; } set { _cas = value; OnPc("Cas"); } }
@@ -98,29 +84,40 @@ namespace Datality {
     /// Combination of raw materials
     /// </summary>
     public class Blend : Chemical {
+        public Blend() {
+            Products = new HashSet<Pro>();
+            Formulinos = new HashSet<Formulino>();
+            Transport = new Transport();
+        }
         public SpecSet SpecSet { get; set; }
-     //   public virtual Safety Safety { get; set; }
-   //     public virtual Transport Transport { get; set; }
+        public virtual Safety Safety { get; set; }
+        public virtual Transport Transport { get; set; }
         public virtual ICollection<Pro> Products { get; set; } = new HashSet<Pro>();
         public virtual ICollection<Formulino> Formulinos { get; set; } = new HashSet<Formulino>();
+    }
+    public class Formulino : Bass {
+        public float Low { get; set; } = 0;
+        public float High { get; set; } = 100;
+        public float Actual { get; set; } = 0;
+        public string Range { get; set; } = "0-100";
+        public virtual Raw Raw { get; set; }
     }
     /// <summary>
     /// Aliasing and branding of a blend
     /// </summary>
     public class Pro : Bass {
+        public Pro() {
+            Blend = new Blend();
+        }
         public string Name { get; set; }
-        public string Class { get { return _class; } set { _class = value; OnPc("Class"); } }
-        private string _class;
-        public string Description { get { return _description; } set { _description = value; OnPc("Description"); } }
-        private string _description;
-        public DateTime IssueDate { get; set; }
-        public DateTime RevisionDate { get; set; }
+        public string Class { get; set; }
+        public string Description { get; set; }
+        //public DateTime IssueDate { get; set; }
+        //public DateTime RevisionDate { get; set; }
         public int Version { get; set; }
-        //public virtual Blend Blend { get; set; }
+        public virtual Blend Blend { get; set; }
         public virtual Brand Brand { get; set; }
     }
-    #endregion
-    #region Documentation
     public class SpecSet : Bass {
         //
     }
@@ -142,8 +139,6 @@ namespace Datality {
         public string Source { get { return _source; } set { _source = value; OnPc("Source"); } }
         private string _source;
     }
-    #endregion
-    #region Branding
     public class Brand : Bass {
         public string Name { get; set; }
         public string FullName { get; set; }
@@ -153,8 +148,6 @@ namespace Datality {
         public string Email { get; set; }
         public string Contact { get; set; }
     }
-    #endregion
-    #region Regulatory
     /// <summary>
     /// Regulation lists 
     /// </summary>
@@ -172,63 +165,4 @@ namespace Datality {
         public Sara Sara { get; set; }
         public LegacyRatings LegacyRatings { get; set; }
     }
-    #endregion
-    #region Bums
-
-    public class Transport : Bass {
-        public bool Hazardous { get; set; } = true;
-        public string Un { get; set; }
-        public string Psn { get; set; }
-        public string ContainsField { get; set; }
-        public string Primary { get; set; }
-        public string Secondary { get; set; }
-        public string PackingGroup { get; set; }
-        public string Type { get; set; } //(air, dot, etc)
-    }
-    [ComplexType]
-    public class Sara {
-        public bool Acute { get; set; } = false;
-        public bool Chronic { get; set; } = false;
-        public bool Fire { get; set; } = false;
-        public bool Pressure { get; set; } = false;
-        public bool Reactivity { get; set; } = false;
-        public Sara313 Sara313 { get; set; }
-    }
- 
-    public class LegacyRatings {
-        public LegacyRating Hmis { get; set; } = new LegacyRating();
-        public LegacyRating Nfpa { get; set; } = new LegacyRating();
-        public LegacyRating Whims { get; set; } = new LegacyRating();
-    }
-    [ComplexType]
-    public class LegacyRating {
-        public int H { get; set; } = 0;
-        public int F { get; set; } = 0;
-        public int R { get; set; } = 0;
-        public string Sp { get; set; } = null;
-    }
-
-    public class Formulino : Bass {
-        public float Low { get; set; } = 0;
-        public float High { get; set; } = 100;
-        public float Actual { get; set; } = 0;
-        public string Range { get; set; } = "0-100";
-        public virtual Raw Raw  { get; set; }
-    }
-    [ComplexType]
-    public class StreetAddress {
-        public string Address1 { get; set; }
-        public string Address2 { get; set; }
-        public string Address3 { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public string Zip { get; set; }
-    }
-    [ComplexType]
-    public class Sara313 {
-        private bool Listed { get; set; } = false;
-        public long Tpq { get; set; }
-        public long Rq { get; set; }
-    }
 }
-#endregion
--- Slashboard/Testival/TestivalofLights.cs
+++ Slashboard/Testival/TestivalofLights.cs
@@ -3,6 +3,7 @@ using System.Data.Entity.Validation;
 using System.Data;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Datality.Minions;
 
 
 namespace Testival {
@@ -24,11 +25,15 @@ namespace Testival {
             //}
         }
 
-        [TestMethod] public void Sixer() {
-            var dt = new Datality.AshleyGraham();
-          
+        [TestMethod] public void IsThisThingOn() {
+            using (var dt = new Datality.AshleyGraham()) {
                 var b = dt.GetValidationErrors();
-         
+                Assert.AreEqual(0, b.Count());
+            }
+        }
+        [TestMethod] public void CourseLoad() {
+            var Mm = new MainMinion();
+            var n = Mm.Pro.Name;
         }
     }
 }
